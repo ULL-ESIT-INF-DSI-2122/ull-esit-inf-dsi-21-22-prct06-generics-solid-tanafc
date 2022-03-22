abd0041f68cfe0f243b5d2ce2bc6445b66e09f77
Tanausú Falcón <alu0101320878@ull.edu.es>
2022-03-22 12:40:13 +0000

    Pruebas clase Pokedex superadas

---
+++ src/ejercicio-1/pokedex.ts
@@ -0,0 +1,46 @@
+import {Fighter} from './fighter';
+
+/**
+ * Stores an entry with the information related to a Fighter
+ */
+type entryFighter = [name: string, attack: number, defense: number,
+    speed: number, hitPoints: number, weight: number, height: number];
+
+/**
+ * Class Pokedex that stores the entries with information about Pokemons
+ */
+export class Pokedex {
+  public entries: entryFighter[] = [];
+
+  constructor(...entries: entryFighter[]) {
+    entries.forEach((entry) => {
+      this.entries.push(entry);
+    });
+  }
+
+  /**
+   * Given certain Fighters, stores the information related to them as an entry
+   * @param fighter Fighters whose entries want to be added
+   */
+  public storeFighters(...fighters: Fighter[]): void {
+    fighters.forEach((fighter) => {
+      this.entries.push([fighter.getName(), fighter.getAttack(), fighter.getDefense(),
+        fighter.getSpeed(), fighter.getHitPoints(), fighter.getWeight(), fighter.getHeight()]);
+    });
+  }
+
+  /**
+   * Given certain names of Figthers, if the Fighter has an entry,
+   * said entry is deleted from the Pokedex
+   * @param fighters Names of the Fighters whose entries want to be deleted
+   */
+  public deleteFighters(...fighters: string[]): void {
+    fighters.forEach((fighter) => {
+      this.entries.forEach((entry, index) => {
+        if (entry[0] == fighter) {
+          this.entries.splice(index, 1);
+        }
+      });
+    });
+  }
+}
---
+++ tests/pokedex.spec.ts
@@ -0,0 +1,44 @@
+import 'mocha';
+import {expect} from 'chai';
+import {Pokedex} from '../src/ejercicio-1/pokedex';
+import {Pokemon} from '../src/ejercicio-1/pokemon';
+import {FireEmblem} from '../src/ejercicio-1/fire_emblem';
+import {Tekken} from '../src/ejercicio-1/tekken';
+import {LeagueOfLegends} from '../src/ejercicio-1/league';
+
+
+describe('Pokedex class function tests', () => {
+  let pokedex = new Pokedex(["Pikachu", 20, 5, 55, 50, 55, 55]);
+
+  it('It creates a new instance of an object with class Pokedex', () => {
+    expect(pokedex).to.be.instanceOf(Pokedex);
+  });
+
+  it('There is an array of entries for the information of Pokemons', () => {
+    expect("entries" in pokedex).to.be.true;
+    expect(pokedex.entries).to.be.eql([["Pikachu", 20, 5, 55, 50, 55, 55]]);
+  });
+
+  it('Stores the data of the given Fighters', () => {
+    let charmander = new Pokemon("Charmander", "Fire", 20, 5, 55, 40, 10, 20);
+    let king = new Tekken("King", "American", "Wrestling", 20, 5, 55, 40);
+    let chrom = new FireEmblem("Chrom", "Sword", 20, 5, 55, 50, 55, 55, false);
+    let veigar = new LeagueOfLegends("Veigar", 0, 50, 10, 30, 40, 55, 10, 10);
+    pokedex.storeFighters(charmander, king, chrom, veigar);
+
+    expect(pokedex.entries).to.be.eql([
+      ["Pikachu", 20, 5, 55, 50, 55, 55],
+      ["Charmander", 20, 5, 55, 40, 10, 20],
+      ["King", 70, 50, 10, 110, 55, 40],
+      ["Chrom", 20, 5, 55, 50, 55, 55],
+      ["Veigar", 50, 40, 40, 55, 10, 10]]);
+  });
+
+  it('Deletes the entries of the Pokemon given its names', () => {
+    pokedex.deleteFighters("Pikachu", "Veigar");
+    expect(pokedex.entries).to.be.eql([
+      ["Charmander", 20, 5, 55, 40, 10, 20],
+      ["King", 70, 50, 10, 110, 55, 40],
+      ["Chrom", 20, 5, 55, 50, 55, 55]]);
+  });
+});
