7d77d8056c31bc5cb6cbab3496f92876538ee198
Tanausú Falcón <alu0101320878@ull.edu.es>
2022-03-23 13:06:39 +0000

    Pruebas para las clases Tekken, Pokemon, FH y LoL

--- tests/combat.spec.ts
+++ tests/combat.spec.ts
@@ -3,30 +3,36 @@ import {expect} from 'chai';
 import {Pokemon} from '../src/ejercicio-1/pokemon';
 import {FireEmblem} from '../src/ejercicio-1/fire_emblem';
 import {Combat} from '../src/ejercicio-1/combat';
+import {Tekken} from '../src/ejercicio-1/tekken';
+import {LeagueOfLegends} from '../src/ejercicio-1/league';
 
 
 describe('Combat class function tests', () => {
   let chikorita = new Pokemon("Chikorita", "Plant", 20, 5, 40, 400, 90, 220, 'Chi', 'Chikorita!');
   let chrom = new FireEmblem("Chrom", "Sword", 20, 5, 55, 500, 90, 300, false, 'You are going down!');
-  let combat = new Combat(chrom, chikorita);
+  let hwoarang = new Tekken("Hwoarang", "South Korea", "Taekwondo", 0, 20, 90, 175, "¿Eso es todo?");
+  let darius = new LeagueOfLegends("Darius", 100, 0, 50, 25, 320, 2050, 80, 190, "Noxus!");
+  let combat1 = new Combat(chrom, chikorita);
+  let combat2 = new Combat(darius, hwoarang);
 
   it('It creates a new instance of an object with class Combat', () => {
-    expect(combat).to.be.instanceOf(Combat);
+    expect(combat1).to.be.instanceOf(Combat);
   });
 
   it('It has two attributes for its combatants', () => {
-    expect([combat.getFirstOpponent(), combat.getSecOpponent()]).to.be.eql([chrom, chikorita]);
+    expect([combat1.getFirstOpponent(), combat1.getSecOpponent()]).to.be.eql([chrom, chikorita]);
   });
 
   it('There is a method for calculating the damage dealt', () => {
-    expect("dmgResult" in combat).to.be.true;
+    expect("dmgResult" in combat1).to.be.true;
   });
 
   it('There is a method to start the combat', () => {
-    expect("start" in combat).to.be.true;
+    expect("start" in combat1).to.be.true;
   });
 
   it('The combat begins correctly and returns the winner', () => {
-    expect(combat.start()).to.be.equal("Chikorita");
+    expect(combat1.start()).to.be.equal("Chikorita");
+    expect(combat2.start()).to.be.equal("Darius");
   });
 });
--- tests/fighter.spec.ts
+++ tests/fighter.spec.ts
@@ -65,3 +65,61 @@ describe('Fire Emblem function tests', () => {
       chrom.getHitPoints()]).to.be.eql([20, 5, 55, 40]);
   });
 });
+
+
+describe('Tekken function tests', () => {
+  let kazuya = new Tekken("Kazuya Mishima", "Japanese", "Karate", 10, 5, 90, 170, "Worthless");
+  let lee = new Tekken("Lee Chaolan", "Japanese", "Mixed", 0, 5, 90, 160, "Excellent!");
+  let eddy = new Tekken("Eddy Gordo", "Brazil", "Capoeira", 0, 15, 90, 180, "Vamos allá!");
+  let steve = new Tekken("Steve", "American", "Boxing", 0, 15, 90, 175, "Wanna try me?");
+
+  it('It creates a new instance of an object with class FireEmblem', () => {
+    expect(kazuya).to.be.instanceOf(Tekken);
+  });
+
+  it('It extends the abstract class Fighter', () => {
+    expect(Tekken.prototype).to.be.instanceOf(Fighter);
+  });
+
+  it('There is an attribute for its name', () => {
+    expect(lee.getName()).to.be.eql("Lee Chaolan");
+    expect(eddy.getName()).to.be.eql("Eddy Gordo");
+  });
+
+  it('There is an attribute for its weight and height', () => {
+    expect([eddy.getWeight(), eddy.getHeight()]).to.be.eql([90, 180]);
+  });
+
+  it('There is an attribute for each of its base stats', () => {
+    expect([steve.getAttack(), steve.getDefense(), steve.getSpeed(),
+      steve.getHitPoints()]).to.be.eql([40, 50, 10, 110]);
+  });
+});
+
+
+describe('LeagueOfLegends function tests', () => {
+  let rakan = new LeagueOfLegends("Rakan", 10, 20, 40, 50, 400, 2050, 70, 180, "Looking good!");
+  let garen = new LeagueOfLegends("Garen", 100, 0, 50, 40, 400, 3050, 70, 180, "Demaciaaa!");
+
+  it('It creates a new instance of an object with class LeagueOfLegends', () => {
+    expect(garen).to.be.instanceOf(LeagueOfLegends);
+  });
+
+  it('It extends the abstract class Fighter', () => {
+    expect(LeagueOfLegends.prototype).to.be.instanceOf(Fighter);
+  });
+
+  it('There is an attribute for its name', () => {
+    expect(garen.getName()).to.be.eql("Garen");
+    expect(rakan.getName()).to.be.eql("Rakan");
+  });
+
+  it('There is an attribute for its weight and height', () => {
+    expect([garen.getWeight(), garen.getHeight()]).to.be.eql([70, 180]);
+  });
+
+  it('There is an attribute for each of its base stats', () => {
+    expect([rakan.getAttack(), rakan.getDefense(), rakan.getSpeed(),
+      rakan.getHitPoints()]).to.be.eql([30, 90, 400, 2050]);
+  });
+});
